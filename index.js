6d04047ebda92b90547c60220abc3d39d3ae3d7b
Prince-R20 <larrysamuel2020@gmail.com>
2025-01-11 09:01:20 +0100

    Showing question on UI 1

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 //Quiz app
-
+const intro = document.getElementById("intro");
+const question_section = document.getElementById("question_section");
+const question_number = document.getElementById("question_number");
+const question = document.getElementById("question");
+const question_option = document.getElementById("question_option")
 //Fetch questions from JSON file
 fetch('questions.json').then(response => {
     if(!response.ok){
         throw new Error("Faield to load questions");
     }
     return response.json().then(data => {
-        console.log(data);
-        quiz(data);
+        Quiz(data);
     })
     .catch(error => {console.error("Error fetching questions:", error)})
 })
 
-function quiz(data){
-    const Quizdata = data;
-    data.forEach(quiz => {
-        console.log(quiz.Question) 
+function startQuiz(){
+    intro.style.display = "none"
+    question_section.style.display = "block"
+}
+
+function Quiz(data){
+    question_number.textContent = `Question ${data[0].num}`
+    const option = data[0].Option
+    option.forEach(element => {
+        const fieldSet = document.createElement("fieldSet");
+        const input = document.createElement("input");
+        const label = document.createElement("label");
+        label.textContent = element;
+        input.type = "checkbox"
+
+        fieldSet.append(input);
+        fieldSet.append(label);
+        question_option.append(fieldSet);
     });
 }
